9c16579041954c509ddcf98d9f7be8ce3138fb5d
NEWEBOA\frank.hsu <frank_hsu@neweb.com.tw>
2022-03-31 15:50:53 +0800

    test_vue3

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/Vue_phoneticnotation/'
-  : '/'
+  ]
 })
